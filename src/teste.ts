a0b1a66a4c69bff889a0aeaf5a94159284263de0
Fukushiro <jpflc2301@gmail.com>
2021-10-12 23:03:33 -0300

    b

--- src/teste.ts
+++ src/teste.ts
@@ -3,17 +3,6 @@ import { sequelize } from './models';
 
 const queryInterface = sequelize.getQueryInterface();
 
-async function testeUp() {
-  await queryInterface.bulkInsert('User', [
-    {
-      username: 'usuario',
-      password: 'senha',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-  ]);
-}
+async function testeUp() {}
 
-async function testeDown() {
-  await queryInterface.bulkDelete('User', null, {});
-}
+async function testeDown() {}
