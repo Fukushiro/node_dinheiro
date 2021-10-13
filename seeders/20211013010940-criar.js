3d71df31e77823942a8ae560192374674791bafe
Fukushiro <jpflc2301@gmail.com>
2021-10-12 23:01:41 -0300

    da

--- seeders/20211013010940-criar.js
+++ seeders/20211013010940-criar.js
@@ -28,6 +28,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-    await queryInterface.bulkDelete('User', null, {});
+    // await queryInterface.bulkDelete('User', null, {});
   },
 };
