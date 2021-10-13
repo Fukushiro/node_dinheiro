c1c9ab835830cde39e4e1a5992ea5bd96d130c09
Fukushiro <jpflc2301@gmail.com>
2021-10-13 00:36:02 -0300

    das

--- config/config.ts
+++ config/config.ts
@@ -15,10 +15,25 @@ const out = {
   productionString: {
     dialect: 'postgres',
     url: envs.DATABASE_URL,
+    native: true,
+    ssl: true,
   },
   development: {
     dialect: 'sqlite',
     storage: 'db_development.sqlite3',
+  },
+  heroku: {
+    dialect: 'postgres',
+    native: true,
+    ssl: true,
+    // dialect: 'postgres',
+    // username: 'vofdcnjzbunwni',
+    // password:
+    //   'f55923a6bd027ff75c69e756bf2eda1c0c97250e34d3fd9e7a388def7c4b0670',
+    // host: 'ec2-54-166-37-125.compute-1.amazonaws.com',
+    // database: 'delteorkpmtfum',
+    // port: '5432',
+    url: 'postgres://vofdcnjzbunwni:f55923a6bd027ff75c69e756bf2eda1c0c97250e34d3fd9e7a388def7c4b0670@ec2-54-166-37-125.compute-1.amazonaws.com:5432/delteorkpmtfum',
   },
 };
 module.exports = out;
