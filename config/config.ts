6d94b36f9a81dbe4fb790f82512a4c24517a6464
Fukushiro <jpflc2301@gmail.com>
2021-10-13 08:58:29 -0300

    das

--- config/config.ts
+++ config/config.ts
@@ -3,7 +3,8 @@ import { envs } from '../config';
 // const config = require('dotenv');
 // config.config();
 
-const out = {
+const out = {};
+module.exports = {
   production: {
     dialect: 'postgres',
     username: 'postgres',
@@ -15,7 +16,7 @@ const out = {
   productionString: {
     dialect: 'postgres',
     url: envs.DATABASE_URL,
-    native: true,
+    // native: true,
     ssl: true,
   },
   development: {
@@ -25,15 +26,14 @@ const out = {
   heroku: {
     dialect: 'postgres',
     native: true,
-    ssl: true,
     // dialect: 'postgres',
-    // username: 'vofdcnjzbunwni',
-    // password:
-    //   'f55923a6bd027ff75c69e756bf2eda1c0c97250e34d3fd9e7a388def7c4b0670',
-    // host: 'ec2-54-166-37-125.compute-1.amazonaws.com',
-    // database: 'delteorkpmtfum',
-    // port: '5432',
-    url: 'postgres://vofdcnjzbunwni:f55923a6bd027ff75c69e756bf2eda1c0c97250e34d3fd9e7a388def7c4b0670@ec2-54-166-37-125.compute-1.amazonaws.com:5432/delteorkpmtfum',
+    username: 'vofdcnjzbunwni',
+    password:
+      'f55923a6bd027ff75c69e756bf2eda1c0c97250e34d3fd9e7a388def7c4b0670',
+    host: 'ec2-54-166-37-125.compute-1.amazonaws.com',
+    database: 'delteorkpmtfum',
+    port: '5432',
+    ssl: true,
+    // url: 'postgres://vofdcnjzbunwni:f55923a6bd027ff75c69e756bf2eda1c0c97250e34d3fd9e7a388def7c4b0670@ec2-54-166-37-125.compute-1.amazonaws.com:5432/delteorkpmtfum?sslmode=require',
   },
 };
-module.exports = out;
